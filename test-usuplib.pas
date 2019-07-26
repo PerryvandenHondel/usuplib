3d75906e383a25ef7c8a04981aeaba6fde7def4a
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-07-26 13:08:39 +0200

    Testing ReadSettingKey()

--- test-usuplib.pas
+++ test-usuplib.pas
@@ -9,5 +9,7 @@ begin
     WriteLn('Your new password is ', GeneratePassword());
     WriteLn('EncloseDoubleQuote(): ', EncloseDoubleQuote('testing double quote text'));
     WriteLn('EncloseSingleQuote(): ', EncloseSingleQuote('testing single quote text'));
+    WriteLn('ReadSettingKey(): ', ReadSettingKey('test-usuplib.conf', 'Settings', 'TestSetting'));
+
     
 end. // of program USupLibTest;
