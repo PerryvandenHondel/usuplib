414b8fc267f91b3f4dae6ed53fb47faf797a16fd
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-07-26 13:08:25 +0200

    Added ReadSettingKey

--- usuplib.pas
+++ usuplib.pas
@@ -15,6 +15,7 @@ unit USupLib;
 interface
 
 
+
 // Generic functions
 function GeneratePassword(): String;                        // Generate a new password in format AAAAbbbb9999!
 
@@ -22,8 +23,16 @@ function GeneratePassword(): String;                        // Generate a new pa
 function EncloseDoubleQuote(const s: string): string;       // Enclose a string with double quotes (")
 function EncloseSingleQuote(const s: string): string;       // Enclose a string with single quotes (')
 
+// Micelanious functions
+function ReadSettingKey(path: AnsiString; section: AnsiString; key: AnsiString): AnsiString;
+
 
 implementation
+
+
+uses
+	sysutils,
+	utextfile;
 
 
 function GeneratePassword(): String;
@@ -100,6 +109,63 @@ begin
 
 	EncloseSingleQuote := r;
 end; // of function EncloseSingleQuote
+
+
+function ReadSettingKey(path: AnsiString; section: AnsiString; key: AnsiString): AnsiString;
+//
+//	Read a Key from a section from a config (.conf) file.
+//
+//	[Section]
+//	Key1=10
+//	Key2=Something
+//
+//	Usage:
+//		WriteLn(ReadSettingKey('file.conf', 'Section', 'Key2'));  > returns 'Something'
+//		When not found, returns a empty string.
+//		
+//	Needs updates for checking, validating data.
+//
+var
+	r: string;					// Return value of this function
+	sectionName: string;
+	inSection: boolean;
+	l: string;					// Line buffer
+	conf: CTextFile;			// Class Text File 
+begin
+	conf := CTextFile.Create(path);
+	conf.OpenFileRead();
+
+	r := '';
+	sectionName := '['+ section + ']';
+	inSection := false;
+	repeat
+		l := conf.ReadFromFile();
+		//WriteLn(inSection, #9, l);
+		
+		if Pos(sectionName, l) > 0 then
+		begin
+			//WriteLn('FOUND SECTION: ', sectionName);
+			inSection := true;
+		end;
+		
+		if inSection = true then
+		
+		begin
+			//if (Pos(key, l) > 0) then
+			// key must start at pos 1
+			if (Pos(key, l) = 1) then
+			begin
+				//WriteLn('Found key ', key, ' found in section ', sectionName);
+				r := RightStr(l, Length(l) - Length(key + '='));
+				//WriteLn(r);
+				Break; // break the loop, r contains now the value of the setting.
+			end; // of if 
+		end; // of if inSection
+		
+	until conf.GetEof();
+	conf.CloseFile();
+	ReadSettingKey := r;
+end; // of function ReadSettingKey
 
 
 end. // of unit usuplib
